96236c6f1ba46facbff23c459307b5ad42483918
Stephen Demos <stphndemos@gmail.com>
2014-10-03 20:29:21 -0400

    Added general structure to Main.hs
    It now has all of the API calls currently planned and functions for
    those calls, and functions for those functions to call. Next commit will
    have even more layers of functions!

--- src/Main.hs
+++ src/Main.hs
@@ -6,19 +6,55 @@ import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
 
+import qualified Data.ByteString as B hiding (map)
+import           Data.Maybe                  (fromMaybe)
+import           Control.Monad.Trans         (liftIO)
+
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
+    ifTop (writeBS "WhatsOpen@RIT") <|>
+    route [ ("open/:time", listOpenLocs)
+          , ("hours/:location/:time", listHoursAtLoc)
+          , ("locations", listLocs)
           ] <|>
     dir "static" (serveDirectory ".")
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+safeGetParam :: MonadSnap f => B.ByteString -> f B.ByteString
+safeGetParam param = fromMaybe "" <$> getParam param
+
+listOpenLocs :: Snap ()
+listOpenLocs = do
+    time <- safeGetParam "time"
+    result <- liftIO $ openAt time
+    writeBS result
+
+-- it's cool that this works. 
+-- it's unfortunate that this won't work with the calls with multiple arguments
+-- which is why I am going to go with the other one, which is equivalent, although wordier
+-- but has the same structure as the multiple argument calls will have
+--listOpenLocs :: Snap ()
+--listOpenLocs = safeGetParam "time" >>= (liftIO . openAt) >>= writeBS
+
+-- this one is literally why they made do notation
+listHoursAtLoc :: Snap ()
+listHoursAtLoc = do
+    location <- safeGetParam "location"
+    time <- safeGetParam "time"
+    result <- liftIO $ hoursAt location time
+    writeBS result
+
+-- this one doesn't use the general layout because it has no arguments
+listLocs :: Snap ()
+listLocs = liftIO locations >>= writeBS
+
+openAt :: B.ByteString -> IO B.ByteString
+openAt = undefined
+
+hoursAt :: B.ByteString -> B.ByteString -> IO B.ByteString
+hoursAt = undefined
+
+locations :: IO B.ByteString
+locations = undefined
